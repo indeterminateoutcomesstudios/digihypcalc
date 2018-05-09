83288bd639ac5bca48d4ebb2b946694f0debb396
Ekin Oral <oralekin@gmail.com>
2018-05-09 08:20:39 +0000

    add leaderboard

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let players = []
+
+  for(let i = 0; i < 10; i++) {
+    players.push({
+      name: "player"+i,
+      id: i
+    });
+  }
+
+  res.render('index', 
+    { title: "express!",
+      players
+    }
+  );
 });
 
 module.exports = router;
